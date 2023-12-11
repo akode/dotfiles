04caba4559e8134d716d04078872d515369dfc75
Andreas Kodewitz <andreas.kodewitz@burda-forward.de>
2023-12-11 12:43:25 +0200

    Brewfile update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,5 +3,9 @@ local wezterm = require 'wezterm'
 return {
   font = wezterm.font 'Hasklug Nerd Font Mono',
   font_size = 13,
-  color_scheme = "Gogh (Gogh)",
+  color_scheme = "Github (base16)",
+  window_frame = {
+    active_titlebar_bg = '#FFFFFF',
+  }
+
 }
