f63802383fc22d9a52ce52b21b15253ff1c2a270
Andreas Kodewitz <andreas.kodewitz@gmail.com>
2025-12-30 18:00:09 +0100

    add mise to install script

--- install.sh
+++ install.sh
@@ -6,8 +6,12 @@ if command -v zsh >/dev/null; then
   sudo chsh -s "$(command -v zsh)" "$USER"
 fi
 
+if ! command -v mise >/dev/null; then
+  sh -c "$(curl https://mise.run/zsh | sh)"
+fi
+
 if ! command -v chezmoi >/dev/null; then
-  sh -c "$(curl -fsLs get.chezmoi.io)" -- init --apply https://github.com/akode/dotfiles.git
+  sh -c "$(curl -fsLs get.chezmoi.io)" -- init --apply akode
 fi
 
 if [ ! -d "$HOME/.zsh" ]; then
